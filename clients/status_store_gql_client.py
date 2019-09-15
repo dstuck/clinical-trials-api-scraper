2e09f0e3c14cc961abbfbafb7854ce14fe261cf0
dstuck <david.e.stuck@gmail.com>
2019-09-15 12:42:18 -0500

    Add proper get or create for institutions

--- clients/status_store_gql_client.py
+++ clients/status_store_gql_client.py
@@ -1,6 +1,6 @@
 import requests
 
-# tmp
+# TODO: productionize this hack
 CONFIG = {
     'graphql': {
         'host': 'localhost',
@@ -25,7 +25,32 @@ class StatusStoreGqlClient(object):
         response.raise_for_status()
         return response.json()
 
-    def create_institution(self, institution_name, org_type):
+    def get_or_create_institution(self, institution_name, org_type):
+        institution_id = self._get_institution(institution_name, org_type)
+        if not institution_id:
+            institution_id = self._create_institution(institution_name, org_type)
+        return institution_id
+
+    def _get_institution(self, institution_name, org_type):
+        """
+        Gets institution by name and type
+        :param institution_name: org_name of institution
+        :param org_type: org_type of institution
+        :return: Id of institution if exists or None
+        """
+        response_json = self.make_request(self._get_institution_query_str(
+            institution_name,
+            org_type,
+        ))
+        edges = response_json['data']['allInstitutions']['edges']
+        if not edges:
+            return None
+        elif len(edges) > 1:
+            raise IndexError("Found more than one institution: {}".format(edges))
+        return edges[0].get('node', {}).get('id')
+
+
+    def _create_institution(self, institution_name, org_type):
         response_json = self.make_request(self._create_institution_query(
             institution_name,
             org_type
@@ -37,13 +62,30 @@ class StatusStoreGqlClient(object):
         return response_json['data']['allInstitutions']
 
     @staticmethod
+    def _get_institution_query_str(institution_name, org_type):
+        return '''query {{
+          allInstitutions(
+            condition:{{
+              orgName: "{org_name}"
+              orgType: "{org_type}"
+            }}
+          ) {{
+            edges {{
+              node {{
+                id
+              }}
+            }}
+          }}
+        }}'''.format(org_name=institution_name, org_type=org_type)
+
+    @staticmethod
     def _create_institution_query(institution_name, org_type):
         return '''mutation {{
       createInstitution(
         input: {{
           institution: {{
-            orgName: "{}",
-            orgType: "{}"
+            orgName: "{org_name}",
+            orgType: "{org_type}"
           }}
         }}
       ) {{
@@ -53,7 +95,7 @@ class StatusStoreGqlClient(object):
           orgType
         }}
       }}
-    }}'''.format(institution_name, org_type)
+    }}'''.format(org_name=institution_name, org_type=org_type)
 
     @staticmethod
     def _get_all_institutions_query_str():
@@ -63,12 +105,17 @@ class StatusStoreGqlClient(object):
               id
               orgName
               orgType
+              lateReportCount
+              readyForReportCount
+              lateReportRate
               trialsByOrgId {
                 nodes {
                   id
                   completionDate
                   completionStatus
                   resultsReportDate
+                  isLate
+                  readyForReport
                 }
               }
             }
