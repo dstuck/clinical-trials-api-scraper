7967925b35b65ddc705a7b8d17e77bbfd375a336
Tom Quisel <tom.quisel@gmail.com>
2019-11-17 17:56:28 -0800

    Make scraper configurable, avoid cron

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.5-alpine
+# Using debian buster because this: https://pythonspeed.com/articles/base-image-python-docker-images/
+FROM python:3.7-slim-buster
 
 WORKDIR /app
 COPY clinical_trials_api_scraper /app/clinical-trials-api-scraper/clinical_trials_api_scraper
@@ -6,10 +7,5 @@ COPY setup.py /app/clinical-trials-api-scraper
 
 RUN pip install /app/clinical-trials-api-scraper
 
-#Set up cron
-COPY daily-trials-scraping /etc/cron.d/daily-trials-scraping
-RUN chmod 0644 /etc/cron.d/daily-trials-scraping
-RUN crontab /etc/cron.d/daily-trials-scraping
-
 # Run scraper once to bootstrap db then set up cron
-CMD update_trials_store.py --max-id 100 && crond -f -l 8
+CMD update_trials_store.py --max-id 100 --host ${GRAPHQL_HOST} --port ${GRAPHQL_PORT} && sleep ${SECONDS_BETWEEN_SCRAPES}
--- clinical_trials_api_scraper/scripts/update_trials_store.py
+++ clinical_trials_api_scraper/scripts/update_trials_store.py
@@ -12,14 +12,16 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def main(min_id=None, max_id=None, in_memory=False):
+def main(min_id=None, max_id=None, in_memory=False, host="localhost", port=5000):
+    logger.info("I am different3")
     if in_memory:
         store = InMemoryTrialsStoreClient()
     else:
-        store = GqlTrialsStoreClient()
+        store = GqlTrialsStoreClient(host, port)
     if not store.is_ready():
         raise ValueError("Store not ready for data")
-    scraper = ClinicalTrialsScraper(data_store_client=store, min_id=min_id, max_id=max_id)
+    scraper = ClinicalTrialsScraper(
+        data_store_client=store, min_id=min_id, max_id=max_id)
     scraper.scrape_all_trials()
     logger.info("Completed scraping trials")
 
@@ -31,10 +33,16 @@ if __name__ == "__main__":
     parser.add_argument("--min-id", type=int, required=False)
     parser.add_argument("--max-id", type=int, required=False)
     parser.add_argument("--in-memory", action='store_true')
+    parser.add_argument("--host")
+    parser.add_argument("--port")
     args = parser.parse_args()
+
+    logger.info(args)
 
     main(
         min_id=args.min_id,
         max_id=args.max_id,
-        in_memory=args.in_memory
+        in_memory=args.in_memory,
+        host=args.host,
+        port=args.port
     )
