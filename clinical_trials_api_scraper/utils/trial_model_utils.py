7a557ee64eda0b3aabc95be94d05907563d9e2a7
Tom Quisel <tom.quisel@gmail.com>
2020-01-01 16:56:20 -0800

    Make scraper write directly to DB (#8)
    * Make scraper configurable, avoid cron
    
    * Remove unneeded log
    
    * Add foundation for scraper that writes via SQL
    
    * Make SQL scraper work better
    
    * Fix issue with missing institution types
    
    * Enhance configurability

--- clinical_trials_api_scraper/utils/trial_model_utils.py
+++ clinical_trials_api_scraper/utils/trial_model_utils.py
@@ -22,11 +22,23 @@ DATE_FIELDS = ['completionDate', 'clinicaltrialsUpdatedAt']
 
 INSTITUTION_FIELDS = ['orgName', 'orgType']
 
+
 def extract_institution_from_trial(trial):
     institution = {}
     for field in INSTITUTION_FIELDS:
         institution[field] = trial.pop(field)
     return institution
+
+
+def split_institution_trial(full_trial):
+    trial = full_trial.copy()
+    institution = {}
+    for field in INSTITUTION_FIELDS:
+        value = trial.pop(field)
+        value = '' if value is None else value
+        institution[field] = value
+    return institution, trial
+
 
 def trial_from_response_data(response_data):
     trial_model = {
@@ -36,5 +48,16 @@ def trial_from_response_data(response_data):
 
     for time_field in DATE_FIELDS:
         if trial_model.get(time_field):
-            trial_model[time_field] = parser.parse(trial_model[time_field]).isoformat()
+            trial_model[time_field] = parser.parse(
+                trial_model[time_field]).isoformat()
     return trial_model
+
+
+def dict_to_snake_case(d):
+    return {to_snake_case(k): v for k, v in d.items()}
+
+
+def to_snake_case(str):
+
+    return ''.join(['_'+i.lower() if i.isupper()
+                    else i for i in str]).lstrip('_')
